227f2ff3fc46ae4fae24633f2cce8d5f3aa39a04
alexgonzalese <alexgonzalese@gmail.com>
2023-08-15 18:47:30 -0500

    Inicio de la Actividad dejo consulta @midudev

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-// Ejercicio 1: crear servidor HTTP con Node
+const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
+
+// 🧑‍💻 @midudev: no entiendo porque me da este mensaje de error
+// 🗒️ TypeError [Error]: Cannot destructure property 'port' of 'server.address(...)' as it is null.
 function startServer () {
-  // return server
+  const desiredPort = process.env.PORT ?? 1234
+  const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    if (req.url === '/') {
+      res.end('<h1>¡Hola mundo!</h1>')
+    } else if (req.url === '/logo.webp') {
+      fs.readFile('./logo.webp', (err, data) => {
+        if (err) {
+          res.statusCode = 500
+          res.end('<h1>500 Internal Server Error</h1>')
+        } else {
+          res.setHeader('Content-Type', 'image/png')
+          res.end(data)
+        }
+      })
+    } else if (req.url === '/contacto') {
+      res.end('<h1>Contacto</h1>')
+    } else {
+      res.statusCode = 404
+      res.end('<h1>404</h1>')
+    }
+  }
+
+  const server = http.createServer(processRequest)
+  server.listen(desiredPort, () => {
+    console.log(`server listening on port http://localhost:${desiredPort}`)
+  })
+
+  return server
 }
 
 module.exports = {
