7e6d48a89eda6412a21be92eb265eb897b8219de
alexgonzalese <alexgonzalese@gmail.com>
2023-08-17 22:09:14 -0500

    Server con node funcionando

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
-const http = require('node:http') // protocolo HTTP
-const fs = require('node:fs')
+const http = require("node:http"); // protocolo HTTP
+const fs = require("node:fs");
 
-// 🧑‍💻 @midudev: no entiendo porque me da este mensaje de error
-// 🗒️ TypeError [Error]: Cannot destructure property 'port' of 'server.address(...)' as it is null.
-function startServer () {
-  const desiredPort = process.env.PORT ?? 1234
-  const processRequest = (req, res) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-    if (req.url === '/') {
-      res.end('<h1>¡Hola mundo!</h1>')
-    } else if (req.url === '/logo.webp') {
-      fs.readFile('./logo.webp', (err, data) => {
-        if (err) {
-          res.statusCode = 500
-          res.end('<h1>500 Internal Server Error</h1>')
-        } else {
-          res.setHeader('Content-Type', 'image/png')
-          res.end(data)
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  const { method, url } = req;
+
+  switch (method) {
+    case "GET":
+      switch (url) {
+        case "/":
+          res.end("<h1>¡Hola mundo!</h1>");
+          break;
+        case "/logo.webp":
+          fs.readFile("./assets/logo.webp", (err, data) => {
+            if (err) {
+              res.statusCode = 500;
+              res.end("<h1>500 Internal Server Error</h1>");
+            } else {
+              res.setHeader("Content-Type", "image/webp");
+              res.end(data);
+            }
+          });
+          break;
+        case "/404":
+          res.statusCode = 404;
+          res.end("<h1>404</h1>");
+          break;
+        default:
+          res.statusCode = 404;
+          res.end("<h1>404</h1>");
+          break;
+      }
+      break;
+    case "POST":
+      switch (url) {
+        case "/contacto": {
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+
+          req.on("end", () => {
+            const { name, email, message } = JSON.parse(body);
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                name,
+                email,
+                message,
+              })
+            );
+          });
+          break;
         }
-      })
-    } else if (req.url === '/contacto') {
-      res.end('<h1>Contacto</h1>')
-    } else {
-      res.statusCode = 404
-      res.end('<h1>404</h1>')
-    }
+
+        default:
+          res.statusCode = 405;
+          res.end("<h1>405</h1>");
+          break;
+      }
+      break;
+    default:
+      res.statusCode = 405;
+      res.end("<h1>405</h1>");
+      break;
   }
+};
 
-  const server = http.createServer(processRequest)
+
+const desiredPort = process.env.PORT ?? 1234;
+function startServer() {
+  const server = http.createServer(processRequest);
   server.listen(desiredPort, () => {
-    console.log(`server listening on port http://localhost:${desiredPort}`)
-  })
+    console.log(`server listening on port http://localhost:${desiredPort}/`);
+  });
 
-  return server
+  return server;
 }
 
 module.exports = {
-  startServer
-}
+  startServer,
+};
